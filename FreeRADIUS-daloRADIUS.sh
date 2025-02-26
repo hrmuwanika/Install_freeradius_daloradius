efab45b1eaa39ac1d96744a1cc1f9f02918b7f94
Henry Robert Muwanika <hrmuwanika@gmail.com>
2025-02-27 00:29:15 +0200

    Update FreeRADIUS-daloRADIUS.sh

--- FreeRADIUS-daloRADIUS.sh
+++ FreeRADIUS-daloRADIUS.sh
@@ -1,47 +1,72 @@
 #!/bin/sh
 
-sudo apt-get update -y
-sudo apt-get upgrade -y
+#--------------------------------------------------
+# Update Server
+#--------------------------------------------------
+echo "============= Update Server ================"
+sudo apt update && sudo apt upgrade -y
+sudo apt autoremove -y
 
-#Install Apache
-sudo apt-get install apache2
+#----------------------------------------------------
+# Disabling password authentication
+#----------------------------------------------------
+echo "Disabling password authentication ... "
+sudo sed -i 's/#ChallengeResponseAuthentication yes/ChallengeResponseAuthentication no/' /etc/ssh/sshd_config
+sudo sed -i 's/UsePAM yes/UsePAM no/' /etc/ssh/sshd_config 
+sudo sed -i 's/#PasswordAuthentication yes/PasswordAuthentication no/' /etc/ssh/sshd_config
+sudo service sshd restart
 
-#Install PHP
-sudo apt-get install php libapache2-mod-php php-gd php-common php-mail php-mail-mime php-mysql php-pear php-db php-mbstring php-xml php-curl
+#--------------------------------------------------
+# Install and configure Firewall
+#--------------------------------------------------
+sudo apt install -y ufw
+sudo ufw allow 22/tcp
+sudo ufw allow 80/tcp
+sudo ufw allow 8000/tcp
+sudo ufw enable 
+sudo ufw reload
 
-#Install MariaDB
+#--------------------------------------------------
+# Set up the timezones
+#--------------------------------------------------
+# set the correct timezone on ubuntu
+timedatectl set-timezone Africa/Kigali
+timedatectl
+
+# Install Apache
+sudo apt -y install apache2 libapache2-mod-php
+sudo systemctl start apache2.service
+sudo systemctl enable apache2.service
+
+# Install PHP
+sudo apt -y install php php-gd php-common php-mail php-mail-mime php-mysql php-pear php-db php-mbstring php-xml php-curl
+
+# Install MariaDB
 sudo apt -y install mariadb-server mariadb-client
+sudo systemctl start mariadb.service
+sudo systemctl enable mariadb.service
 
-
-#Configure Database for FreeRADIUS
+# Configure Database for FreeRADIUS
 sudo mysql_secure_installation
 
-Enter current password for root (enter for none): Just press the Enter
-Set root password? [Y/n]: Y
-New password: Enter password
-Re-enter new password: Repeat password
-Remove anonymous users? [Y/n]: Y
-Disallow root login remotely? [Y/n]: Y
-Remove test database and access to it? [Y/n]:  Y
-Reload privilege tables now? [Y/n]:  Y
+sudo mysql -uroot --password="" -e "CREATE DATABASE radius CHARACTER SET UTF8 COLLATE UTF8_BIN;"
+sudo mysql -uroot --password="" -e "CREATE USER 'radiususer'@'%' IDENTIFIED BY 'G@s%w&rJ';"
+sudo mysql -uroot --password="" -e "GRANT ALL PRIVILEGES ON radius.* TO 'radiususer'@'%';"
+sudo mysql -uroot --password="" -e "FLUSH PRIVILEGES;"
+sudo mysqladmin -uroot --password="" reload 2>/dev/null
 
-sudo mysql -u root -p
-CREATE DATABASE radius CHARACTER SET UTF8 COLLATE UTF8_BIN;
-CREATE USER 'radius'@'%' IDENTIFIED BY 'G@s%w&rJ';
-GRANT ALL PRIVILEGES ON radius.* TO 'radius'@'%';
-QUIT;
+sudo systemctl restart mysql.service
 
-
-#Install FreeRADIUS
-sudo apt policy freeradius
+# Install FreeRADIUS
+sudo apt policy freeradius -y
 sudo apt -y install freeradius freeradius-mysql freeradius-utils
 
-#Once installed, import the freeradius MySQL database schema with the following command:
+# Once installed, import the freeradius MySQL database schema with the following command:
 sudo su -
 sudo mysql -u root -p radius < /etc/freeradius/3.0/mods-config/sql/main/mysql/schema.sql
-sudo  mysql -u root -p -e "use radius;show tables;"
+sudo mysql -u root -p -e "use radius;show tables;"
 
-#Create a soft link for sql module under
+# Create a soft link for sql module under
 sudo ln -s /etc/freeradius/3.0/mods-available/sql /etc/freeradius/3.0/mods-enabled/
 
 #Run the following commands to create the Certificate Authority (CA) keys:
@@ -50,7 +75,7 @@ sudo openssl genrsa 2048 > ca-key.pem
 sudo openssl req -sha1 -new -x509 -nodes -days 3650 -key ca-key.pem > ca-cert.pem
 
 #Make the following changes as per your database:
-sudo nano /etc/freeradius/3.0/mods-enabled/sql
+sudo cat <<EOF > /etc/freeradius/3.0/mods-enabled/sql
 
 sql {
 driver = "rlm_sql_mysql"
@@ -82,17 +107,16 @@ radius_db = "radius"
 
 read_clients = yes
 client_table = "nas"
+EOF
 
-#Save and close
-
-#Set the proper permission
+# Set the proper permission
 sudo chgrp -h freerad /etc/freeradius/3.0/mods-available/sql
 sudo chown -R freerad:freerad /etc/freeradius/3.0/mods-enabled/sql
 
 sudo systemctl restart freeradius
 sudo systemctl status freeradius
 
-#Install daloRADIUS
+# Install daloRADIUS
 sudo apt -y install wget unzip
 wget https://github.com/lirantal/daloradius/archive/master.zip
 unzip master.zip
@@ -114,25 +138,26 @@ sudo chmod 664 /var/www/html/daloradius/library/daloradius.conf.php
 sudo nano /var/www/html/daloradius/library/daloradius.conf.php
 
 #Make the following changes that match your database:
-sudo nano /var/www/html/daloradius/library/daloradius.conf.php
+sudo tee -a /var/www/html/daloradius/library/daloradius.conf.php <<EOF
 
 $configValues['CONFIG_DB_HOST'] = 'localhost';
 $configValues['CONFIG_DB_PORT'] = '3306';
 $configValues['CONFIG_DB_USER'] = 'radius';
 $configValues['CONFIG_DB_PASS'] = 'G@s%w&rJ';
 $configValues['CONFIG_DB_NAME'] = 'radius';
+EOF
 
-#Import the daloRAIUS MySQL tables to the FreeRADIUS database
+# Import the daloRAIUS MySQL tables to the FreeRADIUS database
 cd /var/www/html/daloradius/
 sudo mysql -u root -p radius < /var/www/html/daloradius/contrib/db/fr2-mysql-daloradius-and-freeradius.sql
 sudo mysql -u root -p radius < /var/www/html/daloradius/contrib/db/mysql-daloradius.sql
 
 sudo chown -R www-data:www-data /var/www/html/daloradius/
-
 sudo systemctl restart freeradius.service apache2
 
-#Visit: 
-http://localhost/daloradius/login.php
+# Visit: 
+clear
+echo "access via http://localhost/daloradius/login.php"
+echo "Username: administrator"
+echo "Password: radius"
 
-Username: administrator
-Password: radius
